db7c305aa98f82ac35e4fd98690ef2022b4fcfae
Luke <Lukegotjellyfish@gmail.com>
2022-10-23 17:47:40 +0100

    Updates prior to ahk module testing

--- Script.py
+++ Script.py
@@ -1,13 +1,21 @@
 import sqlite3
+import os
+import glob
+from ahk import AHK
 
 
 class Database:
+    novel_library_path = ""
+
     def __init__(self, value):
         self.db = value
         self.dbCursor = value.cursor()
 
-def scan_novels():
-    pass
+
+def scan_novels(database):
+    for folder in os.listdir(database.novel_library_path):
+        for novel in os.listdir(f"{database.novel_library_path}/{folder}"):
+            print(novel)
 
 
 def update_novels():
@@ -24,27 +32,25 @@ def export_novel_links():
 
 
 def main():
+    ahk.win_get("C:\Windows\System32\cmd.exe - lncrawl")
     database = Database(sqlite3.connect("Novel_Database.db"))
-    novel_library_path = "G:\Books\Novels\Lncrawl\Lightnovels"
+    database.novel_library_path = "G:/Books/Novels/Lncrawl/Lightnovels"
 
     while True:
         print("[1] - Scan lightnovel-crawler path\n" +
               "[2] - Update Novels\n" +
               "[3] - Add Novel\n" +
               "[4] - Export Novel Links")
-        result = input(f"Select Function: ")
+        result = int(input(f"Select Function: "))
+
         if result == 1:
-            scan_novels()
+            scan_novels(database)
         if result == 2:
             update_novels()
         if result == 3:
             add_novel()
         if result == 4:
             export_novel_links()
-
-    while True:
-        result = input(f"Enter Novel Title To add:" +
-              f"")
 
 
 if __name__ == '__main__':
