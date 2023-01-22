903b7b4a5092a61f2f37b2600e8bcc658ed1a26e
Luke Roper <lukegotjellyfish@gmail.com>
2023-01-22 09:56:48 +0000

    Delete novelsave_exports directory

--- novelsave_exports/adb-push-folders.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-:loop
-rem python "C:\Users\Lukeg\Desktop\better-adb-sync-master\src\adbsync.py" --show-progress %1 "/storage/3934-3333/Lncrawl/novelsave/"
-adb push --sync %1 "/storage/3934-3333/Lncrawl/novelsave/"
-shift
-if not "%~1"=="" goto loop
-
-PAUSE
--- novelsave_exports/download-exported-epubs-from-docker-and-fasthosts.bat
+++
@@ -1,2 +0,0 @@
-docker cp novelsave_ongoing_novels_1:/app/novelsave_exports "C:\Users\Lukeg\Desktop\GitHub\MyRepos\novelsave-manager\novelsave_exports"
-scp -r garrus@109.228.37.197:/home/garrus/NovelSave/novelsave_exports "C:\Users\Lukeg\Desktop\GitHub\MyRepos\novelsave-manager"
