6d104595aefbc1f11622f53416ed54730da63c2b
GarrusPC <lukegotjellyfish@gmail.com>
2023-01-22 09:41:10 +0000

    Further sorting

--- pull_docker_packed_epubs.bat
+++
@@ -1,4 +0,0 @@
-docker cp novelsave_completed_novels:/app/novelsave_exports ./novelsave-epubs/
-docker cp novelsave_ongoing_novels_1:/app/novelsave_exports ./novelsave-epubs/
-docker cp novelsave_ongoing_novels_2:/app/novelsave_exports ./novelsave-epubs/
-PAUSE
